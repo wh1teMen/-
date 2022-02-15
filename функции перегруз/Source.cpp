aa4375a4a3d8008055bdaf0e1a0d7a807d8db9a9
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-02-15 20:54:17 +0500

    добавил т1

--- функции перегруз/Source.cpp
+++ функции перегруз/Source.cpp
@@ -12,19 +12,19 @@ double func(double num1, double num2) {
 }
 
 //Шаблонная функция
-template<typename T1,typename T2> T1 maxFunc(T num1, T2 num2) {
+template<typename T1,typename T2> T1 maxFunc(T1 num1, T2 num2) {
 	return num1 > num2 ? num1 : num2;
 }
 int main() {
 	setlocale(LC_ALL, "ru");
 	float n,m;
 	cout << "введите число: ";
-	cin >> n>>m;/*
+	cin >> n>>m;
+
 	cout << func(n,m)<<endl;//cout<<(n>m?n:m)
-	cout << func(5.5,9.5)<<endl;*/
+	cout << func(5.5,9.5)<<endl;
 
-	cout<<maxFunc(n, m);
-
+	
 
 
 
